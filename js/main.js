00a358c2deebd8a86044a77142745c712e3ce6e9
Star-Far7 <starfar98@gmail.com>
2021-11-22 19:08:23 +0300

    fix: slider in html and css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Слайдер
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
@@ -9,6 +10,7 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+// Функция перелистывания слайдера с помощью стрелок влево и вправо
 window.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") {
     document.querySelector(".swiper-button--next").click();
