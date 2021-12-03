da4bcd415de048301977c4dae77af45ccce7ba5d
Star-Far7 <starfar98@gmail.com>
2021-12-03 16:35:46 +0300

    create: added PHPMailer & modal window

--- js/main.js
+++ js/main.js
@@ -1,47 +1,78 @@
-// Слайдер
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  // Слайдер
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
 
-  // Эффект перелистывания слайда
-  effect: "coverflow",
+    // Эффект перелистывания слайда
+    effect: "coverflow",
 
-  // Прелистывания слайдера с помощью стрелок влево и вправо
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    // Прелистывания слайдера с помощью стрелок влево и вправо
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  // Слайдер
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    effect: "flip",
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+  // Эффект паралакса
+  $(".newslatter").parallax({
+    imageSrc: "img/newslatter-bg.jpeg",
+    speed: 0.4,
+  });
+
+  // Модальная кнопка
+  const menuButton = $(".menu-button");
+  // Функция вызова меню
+  menuButton.on("click", () =>
+    $(".navbar-battom").toggleClass("navbar-battom--visible")
+  );
+
+  //Модальная кнопка
+  const modalButton = $('[data-toggle="modal"]');
+  const closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  // Функция вызова
+  function openModal() {
+    const modalOverlay = $(".modal__overlay");
+    const modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  // Функция закрытия
+  function closeModal(event) {
+    event.preventDefault();
+    const modalOverlay = $(".modal__overlay");
+    const modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  // Закрытие окна нажатием на Escape
+  $(document).keyup(function (e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+      const modalOverlay = $(".modal__overlay");
+      const modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
 });
-
-// Слайдер
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  effect: "flip",
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
-
-// Эффект паралакса
-$(".newslatter").parallax({
-  imageSrc: "img/newslatter-bg.jpeg",
-  speed: 0.4,
-});
-
-// Модальнаяа кнопка
-const menuButton = document.querySelector(".menu-button");
-// Функция вызова меню
-menuButton.addEventListener("click", () =>
-  document
-    .querySelector(".navbar-battom")
-    .classList.toggle("navbar-battom--visible")
-);
